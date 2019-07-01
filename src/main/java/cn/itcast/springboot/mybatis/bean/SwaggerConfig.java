c250794410484f01b33e5a53b208bb717672b328
root <1711190101@qq.com>
2019-07-01 14:54:16 +0800

    修改Swagger配置文件

--- src/main/java/cn/itcast/springboot/mybatis/bean/SwaggerConfig.java
+++ src/main/java/cn/itcast/springboot/mybatis/bean/SwaggerConfig.java
@@ -22,7 +22,7 @@ public class SwaggerConfig {
 	 * @return
 	 */
 	@Bean
-	public Docket productApi() {
+	/*public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("cn.itcast.springboot.mybatis"))
@@ -30,7 +30,7 @@ public class SwaggerConfig {
 				.build()
 				.apiInfo(metaData());
 	}
-
+*/
 
 
 	private ApiInfo metaData() {

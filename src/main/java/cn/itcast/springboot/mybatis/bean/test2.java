08b12b2b57a6285d2c935e6f9bf306d0f2d9fc32
root <1711190101@qq.com>
2019-07-01 15:23:34 +0800

    修改配置文件

--- src/main/java/cn/itcast/springboot/mybatis/bean/test2.java
+++ src/main/java/cn/itcast/springboot/mybatis/bean/test2.java
@@ -1,9 +1,14 @@
 package cn.itcast.springboot.mybatis.bean;
 
+import cn.itcast.springboot.mybatis.entity.AliEntity;
+
 public class test2 {
 
     public void test() {
-
+        AliEntity aliEntity = new AliEntity();
+        aliEntity.setDate("2019-06-26");
+        aliEntity.setMeterMoney(20.36D);
+        aliEntity.setPhoneNumbers("18838959376");
     }
 
 }
